dc46758c06437ca5db02546fab0e27ebabb527e3
Anderneo <anderneo.nardi@gmail.com>
2022-07-28 19:37:01 -0300

    feat:criado método get e corrigido toda estrutura do endpoit Livro

---
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/resource/LivroResource.java
@@ -0,0 +1,62 @@
+package com.eternos.magiadoslivros.domain.resource;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eternos.magiadoslivros.domain.model.Livro;
+import com.eternos.magiadoslivros.domain.service.LivroService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping(value = "livro")
+@AllArgsConstructor
+public class LivroResource {
+    
+    private final LivroService livroService;
+
+    @PostMapping
+    public Livro salvar(@RequestBody @Valid LivroRequest livroRequest){
+        return livroService.salvar(livroRequest);
+    }
+
+    //PARAMETROS FORNECIDOS PELO QUERY STRING
+    @GetMapping(path = "todos")
+    public List<Livro>  buscar() {
+        return livroService.buscarTodos();
+    }
+
+    // PARAMETROS FORNECIDOS PELO PATHVARIABEL
+    /*@GetMapping(path = "{id:[0-9]+}")
+    public Sala buscarTodos(@PathVariable Long id){
+        return salaService.buscarPorIdOuFalhar(id);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletar(@PathVariable Long id){
+        salaService.deletar(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public String update(@PathVariable Long id, @RequestBody SalaRequest salaRequest){
+        return "id igual " + id + " obj: " + salaRequest;
+    }
+
+    @PatchMapping(path = "nome/{id}")
+    public String atualizarUnico(@PathVariable Long id, @RequestBody String nome ){
+        return "id=" + id + " nome = " + nome;
+    }*/
+
+}
+
+
