c1c2cfb73c88778eb5b79f00afbdb2e1f81ad56c
Anderneo <anderneo.nardi@gmail.com>
2022-08-02 16:52:41 -0300

    feat:foi criado as classes de dominio de cada model com base no dominio de Livro

--- magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/resource/FornecedorResource.java
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/resource/FornecedorResource.java
@@ -2,11 +2,16 @@ package com.eternos.magiadoslivros.domain.resource;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eternos.magiadoslivros.domain.model.Fornecedor;
+import com.eternos.magiadoslivros.domain.request.FornecedorRequest;
 import com.eternos.magiadoslivros.domain.service.FornecedorService;
 
 import lombok.AllArgsConstructor;
@@ -17,10 +22,15 @@ import lombok.AllArgsConstructor;
 public class FornecedorResource {
 
     private final FornecedorService fornecedorService;
-    
+
+    @PostMapping
+    public Fornecedor salvar(@RequestBody @Valid FornecedorRequest fornecedorRequest){
+        return fornecedorService.salvar(fornecedorRequest);
+    }
+
+    //PARAMETROS FORNECIDOS PELO QUERY STRING
     @GetMapping(path = "todos")
-    public List<Fornecedor>  buscar() {
+    public List<Fornecedor> buscar() {
         return fornecedorService.buscarTodos();
     }
-    
 }
