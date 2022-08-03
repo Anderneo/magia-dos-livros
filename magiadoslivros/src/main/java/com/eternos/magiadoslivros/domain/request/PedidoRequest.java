f2429d0bba77d25d0f08de949f6fb5f7e08dca30
Anderneo <anderneo.nardi@gmail.com>
2022-08-03 14:29:28 -0300

    feat: removido modificador de acesso dos atributos.

--- magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/request/PedidoRequest.java
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/request/PedidoRequest.java
@@ -8,14 +8,15 @@ import lombok.Data;
 
 @Data
 public class PedidoRequest {
-    private Double valorVenda;
-    private String enderecoEntrega;
-    private String formaDePgto;
-    private Integer parcela;
-    private LocalDate dataVenda;
-    private LocalDate dataPgto;
-    private LocalDate dataEntrega;
-    private Boolean vendaCancelada;
-    private Usuario idUsuario;
+    
+    Double valorVenda;
+    String enderecoEntrega;
+    String formaDePgto;
+    Integer parcela;
+    LocalDate dataVenda;
+    LocalDate dataPgto;
+    LocalDate dataEntrega;
+    Boolean vendaCancelada;
+    Usuario idUsuario;
 
 }
