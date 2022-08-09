e0d67835ee1f4cf4aba952db3b5427bc9e199f41
Anderneo <anderneo.nardi@gmail.com>
2022-08-09 08:14:34 -0300

    feat: criado endpoint buscar fornecedor por razao social

--- magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/resource/FornecedorResource.java
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/resource/FornecedorResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eternos.magiadoslivros.domain.model.Fornecedor;
@@ -28,7 +30,11 @@ public class FornecedorResource {
         return fornecedorService.salvar(fornecedorRequest);
     }
 
-    //PARAMETROS FORNECIDOS PELO QUERY STRING
+    @GetMapping(path = "buscar")
+    public Fornecedor buscarRazaoSocial(@RequestParam String razaosocial ){
+        return fornecedorService.buscarRazaoSocial(razaosocial);
+    }
+
     @GetMapping(path = "todos")
     public List<Fornecedor> buscar() {
         return fornecedorService.buscarTodos();
