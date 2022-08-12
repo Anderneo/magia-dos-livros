d175508299e9818011391dc6a1f09fd74f57f597
Anderneo <anderneo.nardi@gmail.com>
2022-08-12 08:34:53 -0300

    feat: alterado caminho para não dar conflito.

--- magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/resource/LivroResource.java
+++ magiadoslivros/src/main/java/com/eternos/magiadoslivros/domain/resource/LivroResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,7 +45,7 @@ public class LivroResource {
         return livroService.buscarNome(nome);
     }
 
-    @DeleteMapping(path = "{id}")
+    @DeleteMapping(path = "deletar/{id}")
     public void deletar(@PathVariable Integer id){
         livroService.deletar(id);
     }
@@ -52,6 +53,11 @@ public class LivroResource {
     @PatchMapping(path = "adicionarestoque/{id}")
     public Livro atualizarEstoque(@PathVariable Integer id, @RequestBody Integer quantLivros ){
         return livroService.atualizarQtdeLivro(id, quantLivros);
+    }
+
+    @PutMapping(path = "atualizar/{id}")
+    public Livro update(@PathVariable("id") Integer id, @RequestBody LivroRequest livroRequest){
+        return livroService.atualizarLivro(id, livroRequest);
     }
 
 }
