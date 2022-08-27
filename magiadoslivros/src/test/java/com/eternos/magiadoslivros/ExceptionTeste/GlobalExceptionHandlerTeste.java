dd4f79fceeadbc561d5d3f01b9575e9db83031ca
Anderneo <anderneo.nardi@gmail.com>
2022-08-27 11:51:28 -0300

    feat:teste da classe Default Exception

---
+++ magiadoslivros/src/test/java/com/eternos/magiadoslivros/ExceptionTeste/GlobalExceptionHandlerTeste.java
@@ -0,0 +1,72 @@
+package com.eternos.magiadoslivros.ExceptionTeste;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import com.eternos.magiadoslivros.domain.exception.DefaultException;
+import com.eternos.magiadoslivros.domain.exception.ErrorResponse;
+import com.eternos.magiadoslivros.domain.exception.GlobalExceptionHandler;
+import com.eternos.magiadoslivros.domain.model.Livro;
+import com.eternos.magiadoslivros.domain.repository.LivroRepository;
+import com.eternos.magiadoslivros.domain.service.LivroService;
+import com.eternos.magiadoslivros.domain.util.LivroUtil;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+
+
+@SpringBootTest
+public class GlobalExceptionHandlerTeste {
+
+    @InjectMocks
+    GlobalExceptionHandler globalExceptionHandle;
+    
+    @Mock
+    LivroRepository livroRepository;
+    
+    @Mock
+    LivroService livroService;
+    
+    @Mock
+    LivroUtil livroUtil;
+    
+    @Mock
+    Livro livro;
+    
+    @Mock
+    MethodArgumentNotValidException methodArgumentNotValidException;
+    
+    @Mock
+    InvalidFormatException invalidFormatException;
+    
+    @Test
+    void TestarhandleNotFoundException(){
+        
+        var mock = DefaultExceptionMock();
+        
+        assertNotNull(mock);
+        assertNotNull(mock.getBody());
+        assertEquals(HttpStatus.NOT_FOUND,mock.getStatusCode());
+        assertEquals(ResponseEntity.class, mock.getClass());
+        assertEquals(HttpStatus.NOT_FOUND, mock.getBody().getHttpStatus());
+        assertEquals(404, mock.getBody().getCode());
+        assertEquals("teste", mock.getBody().getMessage());
+        
+    }
+    
+    private ResponseEntity<ErrorResponse> DefaultExceptionMock() {
+    
+        DefaultException exception = new DefaultException(HttpStatus.NOT_FOUND, "teste");
+    
+        return new ResponseEntity<ErrorResponse>(exception.getErrorResponse(), exception.httpStatus);
+        
+    }
+    
+}
