11ae44ef1fae552889287417bfc3d62fcff6df38
Anderneo <anderneo.nardi@gmail.com>
2022-08-29 23:26:02 -0300

    feat:removido imports

--- magiadoslivros/src/test/java/com/eternos/magiadoslivros/PedidoTeste/PedidoResourceTeste.java
+++ magiadoslivros/src/test/java/com/eternos/magiadoslivros/PedidoTeste/PedidoResourceTeste.java
@@ -4,13 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
--- magiadoslivros/src/test/java/com/eternos/magiadoslivros/PedidoTeste/PedidoUtilTeste.java
+++ magiadoslivros/src/test/java/com/eternos/magiadoslivros/PedidoTeste/PedidoUtilTeste.java
@@ -4,18 +4,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
