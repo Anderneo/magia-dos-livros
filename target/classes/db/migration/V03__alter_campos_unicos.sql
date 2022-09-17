6970961400aa1dc3f6f70ccdbe2d25bb6aed4b7f
Ana Rodrigues <89550269+AnaPRodrigues@users.noreply.github.com>
2022-09-17 18:25:40 -0300

    Delete target directory

--- target/classes/db/migration/V03__alter_campos_unicos.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE usuario
-ADD CONSTRAINT UC_Usuario UNIQUE (rg,cpf);
-
-ALTER TABLE fornecedor
-ADD CONSTRAINT UC_Fornecedor UNIQUE (razao_social,cnpj);
-
-ALTER TABLE livro
-ADD CONSTRAINT UC_Livro UNIQUE (isbn);
-
