f15564ef047e5abec13b9f8a6b61315bdf9c233c
Anderneo <anderneo.nardi@gmail.com>
2022-09-17 12:42:29 -0300

    feat: ddl  convertido

--- src/main/resources/db/migration/V02__insert_fornecedor.sql
+++ src/main/resources/db/migration/V02__insert_fornecedor.sql
@@ -1,178 +1,229 @@
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Teste do Livro', 'Testando Livro SA', 
 '02.514.876/001-01', '1234', 'Rua testei, n1234', '12345678', 'teste1@teste.com', 'Se funcionar já era!!!'); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Grupo Todolivro', 'TODOLIVRO DISTRIBUIDORA LTDA', 
 '03.163.884/0001-93', 'SC', 'Rua das Missões, 696 - Pintora Aguda', '(47)3221-2222', 'atendimento@todolivro.com.br', ''); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('A Página – Distribuidora de Livros', 'A PAGINA DISTRIBUIDORA DE LIVROS LTDA', 
 '01.795.809/0001-10', 'PR', 'Major Fabriciano do Rego Barros, 1050, Hauer', '(41)3213–5600', 'atendimento@apaginadistribuidora.com.br', '');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Livros para Revenda', 'Livros Para Revenda LTDA', 
 '04.552.534/0001-10', 'MS', 'Rua 14 de Julho 800 (Depósito) CG-MS', '(67)992127935', 'pedidos@livrospararevenda.com.br', ''); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Maravilha Livros', 'Maravilha Comércio de livros LTDA', 
 '77.952.800/0001-51', 'PR', 'Rebouças, 3113, Rebouças - PR', '(41)3330-8400', 'atendimento@maravilhalivros.com.br', ''); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Ciranda Cultural', 'Ciranda Cultural LTDA', 
 '68.216.860/0001-09', 'SP', 'Rua José Albino Pereira, 54, galpão 1 - Jardim Alvorada - Polo Industrial - Jandira/SP', '(11) 3761-9500', 'falecom@cirandacultural.com.br', ''); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Plenitude Distribuidora no Atacado', 'Plenitude Distribuidora de Livros e Biblias Ltda', 
 '24.935.400/0001-57', 'SP', 'Rua Nazareno Mingoni, 885 , Jd. do Lago, campinas', '(19) 4042–6336', 'sac@plenitudedistribuidora.com.br', ''); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Toplivros Distribuidora', 'Toplivros Distribuidora -LTDA', 
 '00.366.681/0001-06', 'PR', 'R. Francisco Nunes, 1563, Prado Velho, Curitiba', '(41) 3023–3232', 'sac@toplivros.com', ''); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Disal', 'DISAL DISTRIBUIDORES ASSOCIADOS DE LIVROS S.A.', 
 '62.277.041.0001-87', 'PR', 'AV. MARGINAL DIREITA DO TIETE, 800', '(11) 3226-3100', 'sac@disal.com.br', ''); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO fornecedor (nome_fantasia, razao_social, cnpj, ins_estadual, 
 endereco, telefone, email, observacao)
 VALUES ('Inovação Distribuidora de Livros', 'Inovação Distribuidora de Livros S.A.', 
 '62.277.041.0001-87', 'PR', 'Rua Conselheiro Ramalho, 719', '(11) 3262-1380', 'contato@inovacaodistribuidora.com.br', ''); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Sabia, 309, Moema - SP', 'camila.teste@gmail.com', 
 '5555-5555', 'Teste01', 'Camila Java', '44.423.232-1', '333.000.000-22', 'MULHERCIS', '2021-12-25', 'USUARIO', '1979-02-10'); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Doutor Arnaldo Junior, 309 , Pinheiros - SP', 'jonathan.thoughworks@gmail.com', 
 '4419-9933', '', 'Jonathan Mires Souza', '11.477.090-6', '465.772.421-42', 'HOMEMCIS', '2021-09-26', 'USUARIO', '1980-09-05');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Brigadeiro Galvão, 566, Alto da Lapa - SP', 'renato.barcelos@gmail.com', 
 '2977-7676', '', 'Renato Barcelos', '45.888.542-1', '778.992.342-12', 'HOMEMCIS', '2022-02-16', 'ADMINISTRADOR', '1967-07-04');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Afonso Celso, 111, Vila Mariana - SP', 'ana.maria_ramos@gmail.com', 
 '5411-1176', '', 'Ana Maria Ribeiro', '43.653.9236-1', '778.992.342-12', 'NAOBINARIO', '2022-09-03', 'ADMINISTRADOR', '1983-09-13');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Alameda Rui, 1000, Lago Claro, Paraná ', 'horacioalmeida@hotmail.com', 
 '3211-5687', '', 'Horácio Almeida dos Santos', '67.432.476-4', '278.459.231-09', 'HOMEMCIS','2020-08-17', 'USUARIO', '1984-07-09');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Copacabana, 1009 - RJ', 'antoniapimental@gmail.com', 
 '3475-1154', '', 'Antonia Bentes', '76.335.712-1', '118.432.666-12', 'MULHERCIS', '2022-01-14', 'ADMINISTRADOR', '1985-04-04'); 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Harmonia, 1111, Vila Madalena', 'deborateles@hotmail.com', 
 '4311-7633', '', 'Debora Teles', '87.231.144-6', '452.773.231-99', 'AGENERO', '2021-05-23', 'USUARIO', '1987-05-20');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Capitao Bastos, 1167, Centro, BH', 'lea_oliveira@gmail.com', 
 '9982-4576', '', 'Lea Oliveira', '11.541.239-1', '653.767.888-12', 'HOMEMTRANS', '2022-03-27',  'USUARIO', '1967-07-04');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Alameda Campinas, 534', 'isadora_fernandes_reis.com', 
 '2977-7676', '', 'Isadora Fernandes Reis ', '12.342.239-1', '545.788.876-78', 'MULHERCIS', '2021-11-20','USUARIO', '1970-09-23');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Barra Funda, 502', 'lia_havas@gmail.com', 
 '6733-1087', '', 'Lia Havas', '43.653.127-1', '453.782.545-56', 'MULHERCIS',  '2021-08-15', 'ADMINISTRADOR', '1967-07-04');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO usuario (endereco, email, telefone, observacao, 
 nome, rg, cpf, genero, data_de_cadastro, perfil , data_de_nascimento)
 VALUES ('Rua Doutor Alburquerque, 504', 'fernando_carvalho@gmail.com', 
 '2977-7676', '', 'Fernanda Carvalho', '52.532.112-9', '745.343.895-55', 'HOMEMCIS', '2020-04-12', 'USUARIO', '1959-08-23');
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO livro ( nome, descricao, localizacao_estoque, isbn, valor_venda, valor_recebimento, quantidade_livro, id_fornecedor)
 VALUES ( "Ikigai", "Viver uma vida plena, longa e feliz?", "SP_00003", 343434403,  24.60 , 29.54, 100, 2 );
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO livro ( nome, descricao, localizacao_estoque, isbn, valor_venda, valor_recebimento, quantidade_livro, id_fornecedor)
 VALUES ( "O Hobbit", "O Hobbit conta a história de Bilbo Bolseiro",
 "SP_00004", 980044403,  59.90 , 60.54, 50, 3 );
  
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO livro ( nome, descricao, localizacao_estoque, isbn, valor_venda, valor_recebimento, quantidade_livro, id_fornecedor)
 VALUES ( "Pai Rico, Pai Pobre", "Dicas financeiras", "SP_00005", 53245403,  29.90 , 30.54, 30, 2 );
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO livro ( nome, descricao, localizacao_estoque, isbn, valor_venda, valor_recebimento, quantidade_livro, id_fornecedor)
 VALUES ( "Os segredos da mente milionária", "Dicas financeiras", "SP_00006", 667244403,  21.78 , 23.54, 80, 5 );
  
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO livro ( nome, descricao, localizacao_estoque, isbn, valor_venda, valor_recebimento, quantidade_livro, id_fornecedor)
 VALUES ( "Minutos de sabedoria", "Dicas espirituais", "SP_00007", 667244404,  21.78 , 22.54, 50, 10 ) ;
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO livro ( nome, descricao, localizacao_estoque, isbn, valor_venda, valor_recebimento, quantidade_livro, id_fornecedor)
 VALUES ( "O poder da cura", "Dicas espirituais", "SP_00008", 664244403,  21.78 , 23.54, 40, 10 ) ;
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO livro ( nome, descricao, localizacao_estoque, isbn, valor_venda, valor_recebimento, quantidade_livro, id_fornecedor)
 VALUES ( "A revolução dos bichos", "Livro de ficção", "SP_00009", 111344403,  34.78 , 35.54, 20, 10 ) ;
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 INSERT INTO livro ( nome, descricao, localizacao_estoque, isbn, valor_venda, valor_recebimento, quantidade_livro, id_fornecedor)
 VALUES ( "A coragem de ser imperfeito", "Livro de auto-ajuda", "SP_00010", 111542403,  44.78 , 45.54, 20, 7 ) ;
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(179.9, 'CREDITO', 'Rua Sabia, 309, Moema', 3, '2022-08-09', '2022-08-07','2022-08-08',false,2);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(85.03, 'CREDITO', 'Rua Sabia, 309, Moema', 3, '2022-04-19', '2022-04-18','2022-04-21',false,2);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(35.76, 'DEBITO', 'Alameda Rui, 1000, Lago Claro, Paraná', 3, '2021-02-06', '2021-02-06','2021-02-08',false,5);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(80.32, 'CREDITO', 'Rua Harmonia, 1111, Vila Madalena', 3, '2022-05-19', '2022-05-19','2022-05-20',false,7);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(50.12, 'CREDITO', 'Rua Barra Funda, 502', 2, '2022-03-27', '2022-03-27','2022-08-28',false,10);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(65.00, 'CREDITO', 'Rua Barra Funda, 502', 2, '2022-06-28', '2022-06-28','2022-06-29',false,10);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(39.35, 'CREDITO', 'Rua Barra Funda, 502', 2, '2022-08-09', '2022-08-07','2022-08-08',false,10);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(40.35, 'DEBITO', 'Rua Copacabana, 1009 - RJ', 1, '2022-07-25', '2022-07-25','2022-07-26',false,5);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido(valor_de_venda, forma_de_pagto, endereco_entrega, parcela, data_venda, data_pagto, data_entrega, cancelada, id_usuario)
 VALUES(40.35, 'DEBITO', 'Rua Brigadeiro Galvão, 566, Alto da Lapa - SP', 1, '2022-01-09', '2022-01-09','2022-01-11',false,3);
 
  
+  -- SQLINES LICENSE FOR EVALUATION USE ONLY
   insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(2,1,3);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(1,2,5);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(4,2,1);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(6,3,2);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(7,3,1);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(8,4,1);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(7,4,1);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(6,4,1);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(1,5,1);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(4,6,1);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(5,6,1);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(5,7,2);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(3,8,4);
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 insert into pedido_livro(id_livro, id_pedido,quantidade)
 values(8,9,1);  
-
